bb100b6333cf13ed515da508025b571d12aa135b
Jeremy <howard55662004@yahoo.com>
2016-09-27 16:19:02 +0800

    add routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var redis = require('./models/redis.js');
 var bodyParser = require('body-parser');
+var mongodb = require('./models/mongodb.js');
 
 var app = express();
 
@@ -13,10 +14,10 @@ app.use(bodyParser.urlencoded({
 //Post owner=xxx&type=xxx&content=xxx[&time=xxx]
 app.post('/', function (req, res){
 	if(!(req.body.owner && req.body.type && req.body.content)){
-		if (["male", "female", "all"].indexOf(req.body.type) === -1){
-			return res.json({code: 0, msg: "Type error"});
-		}
-			return res.json({code: 0, msg: "Information is not complete"});
+		return res.json({code: 0, msg: "Information is not complete"});
+	}
+	if (req.body.type && (["male", "female"].indexOf(req.body.type) === -1)){
+		return res.json({code: 0, msg: "Type error"});
 	}
 	redis.throw(req.body, function (result){
 		res.json(result);
@@ -28,10 +29,54 @@ app.get('/', function (req, res){
 	if(!req.query.user){
 		return res.json({code: 0, msg: "Information is not complete"});
 	}
-	if (["male", "female", "all"].indexOf(req.query.type) === -1){
+	if (req.query.type && (["male", "female", "all"].indexOf(req.query.type) === -1)){
 		return res.json({code: 0, msg: "Type error"});
 	}
 	redis.pick(req.query, function (result){
+		if (result.code === 1){
+			mongodb.save(req.query.user, result.msg, function (err){
+				if(err){
+					return res.json({code: 0, msg: "Fail, please try again"});
+				}
+				return res.json(result);
+			});
+		}
+		else{
+			res.json(result);
+		}
+	});
+});
+
+//POST owner=xxx&type=xxx&content=xxx&time=xxx
+app.post('/back', function(req, res){
+	redis.throwBack(req.body, function (result){
+		res.json(result);
+	});
+});
+
+app.get('/user/:user', function(req, res){
+	mongodb.getAll(req.params.user, function (result){
+		res.json(result);
+	});
+});
+
+app.get('/bottle/:_id', function (req,res){
+	mongodb.getOne(req.params._id, function (result){
+		res.json(result);
+	});
+});
+
+app.post('/reply/:_id', function (req, res){
+	if(!(req.body.user && req.body.content)){
+		res.json({code: 0, msg: "Reply information is not complete !"});
+	}
+	mongodb.reply(req.params._id, req.body, function (result){
+		res.json(result);
+	});
+});
+
+app.get('/delete/:_id', function (req, res){
+	mongodb.delete(req.params._id, function (result){
 		res.json(result);
 	});
 });
